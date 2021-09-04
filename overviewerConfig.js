d02b796c0d64cac5fe71d6b6b94032273dcc7bb6
Gacha-Shrimps <gachashrimpsminecraft@gmail.com>
2021-09-04 16:25:26 +0800

    commit singular files

---
+++ overviewerConfig.js
@@ -0,0 +1,146 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "Gacha Shrimps - overworld",
+        "Gacha Shrimps - nether",
+        "Gacha Shrimps - end"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1630732558",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Overworld Day",
+            "zoomLevels": 10,
+            "defaultZoom": 3,
+            "maxZoom": 10,
+            "path": "overworld_day",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Gacha Shrimps - overworld",
+            "last_rendertime": 1630628690,
+            "imgextension": "jpg",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                16,
+                96,
+                32
+            ],
+            "minZoom": 0,
+            "spawn": [
+                16,
+                96,
+                32
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Overworld Night",
+            "zoomLevels": 10,
+            "defaultZoom": 3,
+            "maxZoom": 10,
+            "path": "overworld_night",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Gacha Shrimps - overworld",
+            "last_rendertime": 1630628690,
+            "imgextension": "jpg",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                16,
+                96,
+                32
+            ],
+            "minZoom": 0,
+            "spawn": [
+                16,
+                96,
+                32
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Nether",
+            "zoomLevels": 8,
+            "defaultZoom": 3,
+            "maxZoom": 8,
+            "path": "nether",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Gacha Shrimps - nether",
+            "last_rendertime": 1630646788,
+            "imgextension": "jpg",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                16,
+                96,
+                32
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 0
+        },
+        {
+            "name": "End",
+            "zoomLevels": 9,
+            "defaultZoom": 3,
+            "maxZoom": 9,
+            "path": "end",
+            "base": "",
+            "bgcolor": "#000000",
+            "world": "Gacha Shrimps - end",
+            "last_rendertime": 1628959714,
+            "imgextension": "jpg",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                16,
+                96,
+                32
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 0
+        }
+    ]
+};
